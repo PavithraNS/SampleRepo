e9f78befe60b74366191ff56d5a258e2eb1e2fef
PavithraNS <nspavi1991@gmail.com>
2021-08-28 08:58:37 +0530

    Adding NewProject

---
+++ ConsoleApp058/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp058
+{
+    class Car
+    {
+        string color = "red";
+        int model = 345737;
+
+        public void Drive()
+        {
+            Console.WriteLine("Driving");
+        }
+
+        public static void Brake()
+        {
+            Console.WriteLine("brake");
+        }
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.model);
+            myObj.Drive();
+            Brake();
+            Console.Read();
+        }
+    }
+}
